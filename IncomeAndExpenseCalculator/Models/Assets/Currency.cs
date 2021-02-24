12ba4dc99a8fd0c4abb50dfe8f36ea41030f7668
evgenii_volkov <ea.volkov97@yandex.ru>
2021-02-24 04:11:35 +0300

    Initial commmit

---
+++ IncomeAndExpenseCalculator/Models/Assets/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Media;
+
+namespace IncomeAndExpenseCalculator.Models.Assets
+{
+    /// <summary>
+    /// Представляет валюту.
+    /// </summary>
+    public class Currency : FinancialAsset
+    {
+        /// <summary>
+        /// Обозначение валюты.
+        /// </summary>
+        public char Symbol { get; private set; }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр валюты.
+        /// </summary>
+        /// <param name="name">Название валюты.</param>
+        /// <param name="symbol">Обозначение валюты.</param>
+        public Currency(string name, char symbol = ' ', ImageSource icon = null, string description = "") : base(name, icon, description)
+        {
+            Symbol = symbol;
+        }
+    }
+}
---
+++ IncomeAndExpenseCalculator/Models/Assets/FinancialAsset.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Media;
+
+namespace IncomeAndExpenseCalculator.Models.Assets
+{
+    /// <summary>
+    /// Представляет абстрактный финансовый актив.
+    /// </summary>
+    public abstract class FinancialAsset
+    {
+        /// <summary>
+        /// Название финансового актива.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Иконка финансового актива.
+        /// </summary>
+        public ImageSource Icon { get; private set; }
+
+        /// <summary>
+        /// Описание финансового актива.
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр финансового актива.
+        /// </summary>
+        /// <param name="name">Название актива.</param>
+        /// <param name="icon">Иконка актива.</param>
+        /// <param name="description">Описание актива.</param>
+        protected FinancialAsset(string name, ImageSource icon = null, string description = "")
+        {
+            Name = name;
+            Icon = icon;
+            Description = description;
+        }
+    }
+}
